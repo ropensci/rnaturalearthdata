f8f25ddf16f2af7cbe04522768813e4ff9a545a8
AndySouth <southandy@gmail.com>
2016-02-16 22:43:46 +0000

    package doc

---
+++ R/rnaturalearthdata-package.r
@@ -0,0 +1,19 @@
+#' rnaturalearthdata : world vector maps from Natural Earth
+#'
+#' Facilitates world mapping by making \href{http://www.naturalearthdata.com/}{Natural Earth} map data more easily available to R users.
+#'
+#' @name rnaturalearthdata
+#' @docType package
+#' @seealso ne_countries
+#' @import sp
+NULL
+
+# Hide variables from R CMD check
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(c("countries110", "countries50",
+                           "map_units110", "map_units50",
+                           "sovereignty110", "sovereignty50",
+                           "coastline110", "coastline50",
+                           "states50"
+                           ))
+}
