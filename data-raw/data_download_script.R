606439495d122e7d508fbc2a8d3f83d31ea84a9c
Philippe Massicotte <pmassicotte@hotmail.com>
2024-02-04 15:40:10 -0500

    convert all data frame columns name to lower case

--- data-raw/data_download_script.R
+++ data-raw/data_download_script.R
@@ -1,7 +1,8 @@
 # data-raw/data_download_script.r
 
 # how data is got into rnaturalearth
-# reproducible workflow allowing package data to be updated e.g. when there are updates to Natural Earth.
+# reproducible workflow allowing package data to be updated e.g. when there are
+# updates to Natural Earth.
 # just by sourcing this script
 
 # one example file in rnaturalearth
@@ -11,29 +12,79 @@
 library(rnaturalearth)
 
 # countries in rnaturalearth for now
-countries110 <- ne_download(scale = 110, type = "countries", category = "cultural")
-map_units110 <- ne_download(scale = 110, type = "map_units", category = "cultural")
-sovereignty110 <- ne_download(scale = 110, type = "sovereignty", category = "cultural")
+countries110 <- ne_download(
+  scale = 110,
+  type = "countries",
+  category = "cultural"
+)
 
-countries50 <- ne_download(scale = 50, type = "countries", category = "cultural")
-map_units50 <- ne_download(scale = 50, type = "map_units", category = "cultural")
-sovereignty50 <- ne_download(scale = 50, type = "sovereignty", category = "cultural")
+map_units110 <- ne_download(
+  scale = 110,
+  type = "map_units",
+  category = "cultural"
+)
+
+sovereignty110 <- ne_download(
+  scale = 110,
+  type = "sovereignty",
+  category = "cultural"
+)
+
+countries50 <- ne_download(
+  scale = 50,
+  type = "countries",
+  category = "cultural"
+)
+
+map_units50 <- ne_download(
+  scale = 50,
+  type = "map_units",
+  category = "cultural"
+)
+
+sovereignty50 <- ne_download(
+  scale = 50,
+  type = "sovereignty",
+  category = "cultural"
+)
 
 states50 <- ne_download(scale = 50, type = "states", category = "cultural")
 
 # points for tiny countries that don't appear in higher res data
 # not available for scale 10
-tiny_countries110 <- ne_download(scale = 110, type = "tiny_countries", category = "cultural")
-# devtools::use_data(tiny_countries110, compress='xz', overwrite=TRUE)
+tiny_countries110 <- ne_download(
+  scale = 110,
+  type = "tiny_countries",
+  category = "cultural"
+)
 
-tiny_countries50 <- ne_download(scale = 50, type = "tiny_countries", category = "cultural")
-# devtools::use_data(tiny_countries50, compress='xz', overwrite=TRUE)
+tiny_countries50 <- ne_download(
+  scale = 50,
+  type = "tiny_countries",
+  category = "cultural"
+)
 
-# used this & then 2 loops at end of script to add into package
-# data_object_names <- c("tiny_countries110", "tiny_countries50")
+coastline110 <- ne_download(
+  scale = 110,
+  type = "coastline",
+  category = "physical"
+)
 
-coastline110 <- ne_download(scale = 110, type = "coastline", category = "physical")
-coastline50 <- ne_download(scale = 50, type = "coastline", category = "physical")
+coastline50 <- ne_download(
+  scale = 50,
+  type = "coastline",
+  category = "physical"
+)
+
+# Loop all df's and tolower() column names
+object_list <- ls()
+
+# Loop through each object
+for (object_name in object_list) {
+  df <- get(object_name)
+  names(df) <- tolower(names(df))
+  assign(object_name, df, envir = .GlobalEnv)
+}
 
 usethis::use_data(map_units110, compress = "xz", overwrite = TRUE)
 usethis::use_data(sovereignty110, compress = "xz", overwrite = TRUE)
